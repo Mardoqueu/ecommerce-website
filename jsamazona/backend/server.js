f87af455f4cc6c943a050b9500def0daf6d11ea9
mardoqueu <mdq767@gmail.com>
2022-09-12 19:10:46 -0300

    Load Products from Backend

--- jsamazona/backend/server.js
+++ jsamazona/backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const data = require('./data.js');
 const app = express();
+app.use(cors());
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 
-app.get("/api/products", (req, res) =>{
-    res.send(data.products);
-
-})
-
-app.listen(5000, ()=> {
-    console.log('serve at http://localhost:5000');
+app.listen(5000, () => {
+  console.log('serve at http://localhost:5000');
 });
