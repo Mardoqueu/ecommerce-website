dd95fc38fc3afa9058408ce66f68448f7076c7c6
mardoqueu <mdq767@gmail.com>
2022-09-13 15:40:22 -0300

    Install and config babel

---
+++ .history/jsamazona/backend/server_20220913153653.js
@@ -0,0 +1,14 @@
+const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import data from './data.js';
+
+const app = express();
+app.use(cors());
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.listen(5000, () => {
+  console.log('serve at http://localhost:5000');
+});
