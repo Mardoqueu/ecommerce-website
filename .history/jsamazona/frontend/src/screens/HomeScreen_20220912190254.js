5aba2550fdccc6568864cdc078accb331e0d018c
mardoqueu <mdq767@gmail.com>
2022-09-13 15:12:02 -0300

    add webpack

---
+++ .history/jsamazona/frontend/src/screens/HomeScreen_20220912190254.js
@@ -0,0 +1,45 @@
+import data from '../data.js';
+const HomeScreen = {
+  render: async () => {
+    const response = await fetch("http://localhost:5000/api/products", {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+
+    if(!response || response.ok){
+      return `<div>Error in getting data</div>`
+    }
+
+    const products = await response.json();
+
+    return `
+    <ul class="products">
+      ${products
+        .map(
+          (product) => `
+      <li>
+        <div class="product">
+          <a href="/#/product/${product._id}">
+            <img src="${product.image}" alt="${product.name}" />
+          </a>
+        <div class="product-name">
+          <a href="/#/product/1">
+            ${product.name}
+          </a>
+        </div>
+        <div class="product-brand">
+          ${product.brand}
+        </div>
+        <div class="product-price">
+          $${product.price}
+        </div>
+        </div>
+      </li>
+      `
+        )
+        .join('\n')}
+    `;
+  },
+};
+export default HomeScreen;
