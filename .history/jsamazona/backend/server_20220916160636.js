d3adceb6843da57c96e24ecb14e4f6a2b8cc9332
mardoqueu <mdq767@gmail.com>
2022-09-19 10:07:34 -0300

    Connect To MongoDB and Create Admin User

---
+++ .history/jsamazona/backend/server_20220916160636.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose, { mongo } from 'mongoose';
+import data from './data';
+import config from './config';
+
+
+mongoose.connect(config.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).catch((error) =>{
+  console.log(error.reason);
+})
+const app = express();
+app.use(cors()); 
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found!' });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('serve at http://localhost:5000');
+});
---
+++ .history/jsamazona/backend/server_20220917110652.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import data from './data';
+import config from './config';
+import userRouter from './routers/userRouter';
+
+mongoose
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to mongodb.');
+  })
+  .catch((error) => {
+    console.log(error.reason);
+  });
+const app = express();
+app.use(cors());
+app.use('/api/users', userRouter);
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found!' });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('serve at http://localhost:5000');
+});
